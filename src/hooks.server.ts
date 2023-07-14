b80856ddf6b5ab64dc776c1e63fb8f8848999128
Alexander Demin <ademin@gmail.com>
2023-07-14 18:33:44 +0100

    Add Auth0

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,22 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import { env } from "$env/dynamic/private";
+import Auth from "@auth/core/providers/auth0";
+import GitHub from "@auth/core/providers/github";
+import { SvelteKitAuth } from "@auth/sveltekit";
+import { ManagementClient } from "auth0";
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: env.GITHUB_ID, clientSecret: env.GITHUB_SECRET }),
+    Auth({
+      clientId: env.AUTH0_ID,
+      clientSecret: env.AUTH0_SECRET,
+      issuer: "https://" + env.AUTH0_DOMAIN
+    }),
+  ],
 })
+
+export const Auth0Management = new ManagementClient({
+    domain: env.AUTH0_DOMAIN,
+    clientId: env.AUTH0_API_ID,
+    clientSecret: env.AUTH0_API_SECRET,
+});
